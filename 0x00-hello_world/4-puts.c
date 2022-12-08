c6d63bfa8abae354451a2e9af122dfe6ba8e925f
scottandee <andeeolayinkscott@gmail.com>
2022-12-08 13:33:34 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+  * main - entry point
+  * void - There is no parameter passed into this function
+  * Description: this is the entry point of our program
+  * Return: The returned value of the function is zero
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
