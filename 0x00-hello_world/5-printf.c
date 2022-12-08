d57fd6fcffd0c806bf4bee1b3d17b912500ac533
scottandee <andeeolayinkscott@gmail.com>
2022-12-08 14:02:21 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main -entry point function
+  * void: no parameters are entered into this function
+  * Description: This the entry point of our program
+  * Return: the function will return a value of zero
+  */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
