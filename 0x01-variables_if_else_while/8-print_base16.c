d631a0ce1e15d05ddf996f8953b44287310e5322
scottandee <andeeolayinkscott@gmail.com>
2022-12-09 19:25:38 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point function
+ * Description: this is the entry point function
+ * Return: the return value is zero
+ */
+int main(void)
+{
+	char i, c;
+
+	for (i = '0'; i <= '9' i++)
+	{
+		putchar(i);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
