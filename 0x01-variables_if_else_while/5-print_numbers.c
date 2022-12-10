6a4f203bdac18efdd5e0657c8dd99e4641f75bb2
scottandee <andeeolayinkscott@gmail.com>
2022-12-09 18:51:30 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point function
+ * Description: this is the entry point function
+ * Return: this function returns a value if zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
