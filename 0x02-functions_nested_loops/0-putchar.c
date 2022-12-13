480c0c7ae10bcb8b91649eacc31aee165009222b
scottandee <andeeolayinkscott@gmail.com>
2022-12-13 15:31:16 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point function
+ * Description: this is the entry point variable
+ * Return: the return value of this function is zero
+ **/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
