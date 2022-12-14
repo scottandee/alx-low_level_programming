9e1602a3c972cf53295f1c32003e4e6a02739706
scottandee <andeeolayinkscott@gmail.com>
2022-12-13 18:54:29 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * Description: this function prints the last digit of the argument
+ * @d: this is the value that we are going to print the last digit of
+ * Return: this will return the value of the last digit
+ **/
+int print_last_digit(int d)
+{
+	d = d % 10;
+	return (d);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int x);
-int print_last_digit(int);
+int print_last_digit(int d);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
