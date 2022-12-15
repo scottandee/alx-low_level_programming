99647a6075fce7d6429631e159468fe7bd714194
scottandee <andeeolayinkscott@gmail.com>
2022-12-15 12:23:17 -0800

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,15 @@ int largest_number(int a, int b, int c)
 
 	if (a < 0)
 	{
-		a = a * -1;
+		a = a * (-1);
 	}
 	else if (b < 0)
 	{
-		b = b * -1;
+		b = b * (-1);
 	}
 	else
 	{
-		c = c * -1;
+		c = c * (-1);
 	}
 
 	if (a > b && b > c)
