e28ffdd9199f82569b286ba188dceb1adc5181fd
scottandee <andeeolayinkscott@gmail.com>
2022-12-19 11:11:37 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strlen - this prints the length of a string
+* @s: this is the string
+*/
+
+int _strlen(char *s)
+{
+	int arr.length;
+
+	arr.length = sizeof(*s) / sizeof(*s[0]);
+	_putchar(arr.length + '0');
+}
