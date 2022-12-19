d57e46af5b848643062e5bb5f6357ddf2a3121ed
scottandee <andeeolayinkscott@gmail.com>
2022-12-19 10:37:05 -0800

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,15 +3,15 @@
 /**
 * swap_int - this swaps the value of two variables with one another
 * @a: this is one of the variables to be swapped
-* @b: this is the secont variable
+* @b: this is the second variable
 */
 
 void swap_int(int *a, int *b)
 {
 	int c, d;
 
-	*a = c;
-	*b = d;
+	c = *a;
+	d = *b;
 
 	*a = d;
 	*b = c;
