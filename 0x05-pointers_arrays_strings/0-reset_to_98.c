536b1a38d854a520404ef75e9dcc4cd007c09c54
scottandee <andeeolayinkscott@gmail.com>
2022-12-19 10:07:55 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,10 @@
 
 /**
 * reset_to_98 - dereferencing with a pointer
-* @*n: pointer to the variable  thats too be dereferenced
+* @n: pointer to the variable  thats too be dereferenced
 */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-	_putchar('\n');
 }
