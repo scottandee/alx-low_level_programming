edd0fe446e2dd47b1fda1cfb8be5cf8fadc43851
scottandee <andeeolayinkscott@gmail.com>
2022-12-19 18:28:41 -0800

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%p, ", *(a + i));
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
 	}
-	printf("\n")
+	printf("\n");
 }
