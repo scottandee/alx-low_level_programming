3b87e793d7e9cee8407b82c1c91f98ad97eeb6fd
scottandee <andeeolayinkscott@gmail.com>
2022-12-22 06:42:21 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* *_strcat - this concatenates two strings
+* @dest: this is the string that'll increase in size
+* @src: this'll be added to dest
+* Return: dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, src_length = 0, dest_length = 0;
+
+	while (src[src_length] != '\0')
+	{
+		length++;
+	}
+	while (dest[dest_length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < src_length; i++)
+	{
+		dest[dest_length + i] = src[i];
+	}
+	return (dest);
+}
