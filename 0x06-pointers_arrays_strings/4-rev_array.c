f13abb54e7c3e82156a09d19a9190ce6510c7283
scottandee <andeeolayinkscott@gmail.com>
2022-12-22 07:52:50 -0800

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* reverse_array - this reverses the content of an array
+* @a: this is the string to be reversed
+* @n: this is the number of elements in the array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, temp, j;
+
+	i = 0;
+	j = n - 1;
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
