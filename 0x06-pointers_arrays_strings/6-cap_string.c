72e6407343ec02d10a32bc4622d7c86adeeac83b
scottandee <andeeolayinkscott@gmail.com>
2022-12-22 20:34:09 -0800

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,17 +2,30 @@
 
 /**
 * *cap_string - this capitalizes the first letter of every word
-* 
-* Return:
+* @s: this is the string to be capitalized
+* Return: s
 */
 
-char *cap_string(char *);
+char *cap_string(char *s)
 {
-	int length = 0;
+	int i, length = 0;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
-	for 90
+	for (i = 0; i < length; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' ||
+			s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' ||
+			s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' ||
+			s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+	}
+	return (s);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,9 +5,9 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+char *string_toupper(char *str);
+char *cap_string(char *s);
+char *leet(char *str);
 int _strcmp(char *s1, char *s2);
 
 #endif /*MAIN_H*/
