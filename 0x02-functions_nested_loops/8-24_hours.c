037160f97400661bc4c5baa3f3dd57edb8b48216
scottandee <andeeolayinkscott@gmail.com>
2022-12-26 23:41:49 -0800

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,10 +14,10 @@ void jack_bauer(void)
 		{
 			_putchar((h / 10) + '0');		
 			_putchar((h % 10) + '0');
-			_putchar(':')
+			_putchar(':');
 			_putchar((m / 10) + '0');		
 			_putchar((m % 10) + '0');
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
