86528230d056d09971828f2f07815b3ac9b450a1
scottandee <andeeolayinkscott@gmail.com>
2022-12-26 23:54:14 -0800

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* times_table - this prints out the nine times table
+*/
+
+void times_table(void)
+{
+	int i, j, times;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			times = i * j;
+			_putchar(i + '0')
+			if (j == 9)
+			{
+				_putchar('\n');
+			}
+			else if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(',');
+			}
+		}
+	}
+}
