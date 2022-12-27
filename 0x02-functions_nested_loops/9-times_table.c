aa586fdd2295e291fd8d610bf9e15dbcc7601ba3
scottandee <andeeolayinkscott@gmail.com>
2022-12-27 01:06:29 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,39 +10,24 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			times = i * j;
-			if (times > 9)
+			if (times <= 9)
 			{
-				if (j == 9)
-				{
-					_putchar((times / 10) + '0');
-					_putchar((times % 10) + '0');
-					_putchar('\n');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(',');
-					_putchar((times / 10) + '0');
-					_putchar((times % 10) + '0');
-				}
+				_putchar(' ');
+				_putchar(times + '0');
 			}
 			else
 			{
-				if (j == 9)
-					_putchar(times + '0');
-					_putchar('\n');
-				else if (j != 0)
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(',');
-					_putchar(times + '0');
-				else
-					_putchar(times + '0');
-					_putchar(',');
+				_putchar((times / 10) + '0');
+				_putchar((times % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
 }
