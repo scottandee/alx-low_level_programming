82db835622dc6b8a47ad0f484a7b3c1a4943a23a
scottandee <andeeolayinkscott@gmail.com>
2023-01-01 10:08:22 -0800

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,12 +11,8 @@ void _puts(char *str)
 
 	while (*(str + length) != '\0')
 	{
+		putchar(str[length]);
 		length++;
-	}
-
-	for (i = 0; i < length; i++)
-	{
-		_putchar(*(str + i));
 	}
 	_putchar('\n');
 }
