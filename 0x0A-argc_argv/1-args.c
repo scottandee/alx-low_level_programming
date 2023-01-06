9bb015f6a370521d41fff89f2bd7c5ea34f4084a
scottandee <andeeolayinkscott@gmail.com>
2023-01-06 05:33:11 -0800

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - this is the main function
+* @argc: this counts the arguments
+* @argv: this is a string containing the arguments
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
+
