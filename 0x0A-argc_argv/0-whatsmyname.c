320d48e0549ba9349845ea4a81634428fc34fe0f
scottandee <andeeolayinkscott@gmail.com>
2023-01-06 02:20:38 -0800

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - this is the main function
+* @argc: this counts the arguments
+* @argv: this is a string containing the arguments
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
+
+
