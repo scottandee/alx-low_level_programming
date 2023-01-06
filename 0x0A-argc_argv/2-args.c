ff65e59071399f1a80a12822aae3a5d9bf4223a5
scottandee <andeeolayinkscott@gmail.com>
2023-01-06 06:08:07 -0800

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - this is the main function
+* @argc: this counts the arguments
+* @argv: this is a string containing the arguments
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+
