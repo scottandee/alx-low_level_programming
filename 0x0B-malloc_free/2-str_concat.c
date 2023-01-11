210dc7d32075f1f958ea3d5625b01dfda1e9cac8
scottandee <andeeolayinkscott@gmail.com>
2023-01-10 18:21:09 -0800

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,14 @@ char *str_concat(char *s1, char *s2)
 	char *array;
 	int i, s1_len = 0, s2_len = 0;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1[s1_len] != '\0')
 	{
 		s1_len++;
