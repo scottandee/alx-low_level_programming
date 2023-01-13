3341437a7ca6801688cf12ec4fc62f454748053a
scottandee <andeeolayinkscott@gmail.com>
2023-01-12 16:42:26 -0800

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,6 +4,7 @@
 /**
 * *malloc_checked - this allocates memory using malloc
 * @b: this is the size of memory that will be allocated
+* Return: 98 if failure and pointer to memory if success
 */
 
 void *malloc_checked(unsigned int b)
@@ -15,5 +16,5 @@ void *malloc_checked(unsigned int b)
 	{
 		exit(98);
 	}
-	return(p);
+	return (p);
 }
