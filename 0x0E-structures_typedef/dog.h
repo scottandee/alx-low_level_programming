cdff2d6c8462d2a8ac2b487b3e2ff2fc08eb508c
scottandee <andeeolayinkscott@gmail.com>
2023-01-16 17:00:56 -0800

    Task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#define _DOG_H
+#ifndef _DOG_H
+
+/**
+* struct dog - dogs and their characteristics
+* @name: dog name
+* @age: dog age
+* @owner: owner name
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif/* _DOG_H */
