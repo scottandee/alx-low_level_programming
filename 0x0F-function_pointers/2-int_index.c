c6685978af083cff0d17699f542d766b2ac1cafe
scottandee <andeeolayinkscott@gmail.com>
2023-01-18 19:18:17 -0800

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,6 +16,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+	if (!array || !cmp)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
