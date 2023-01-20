589d5d74b356305a86943498ca7168548d64d810
scottandee <andeeolayinkscott@gmail.com>
2023-01-19 18:34:10 -0800

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,6 +16,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list param;
 
 	va_start(param, n);
+	if (n == 0)
+	{
+		return;
+	}
 	for (i = 0; i < n; i++)
 	{
 		if (separator == NULL)
