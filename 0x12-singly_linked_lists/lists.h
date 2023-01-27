56390073e692de0d8316fe8adcc31d022001375f
scottandee <andeeolayinkscott@gmail.com>
2023-01-26 19:00:33 -0800

    Task 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _LISTS_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,5 +20,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /*_LISTS_H*/
