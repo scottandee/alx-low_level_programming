73416747469f2a77a97e1a2eb8c560950cc67d29
Andee <andeeolayinkascott@gmail.com>
2023-02-01 04:05:00 +0000

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,13 +9,15 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int data;
 
 	if (head == NULL)
 	{
 		return (0);
 	}
 	temp = *head;
+	data = (*head)->n;
 	*head = (*head)->next;
 	free(temp);
-	return ((*head)->n);
+	return (data);
 }
