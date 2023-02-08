d0cc6a412ec0d53834b4517af341814f7435111b
Andee <andeeolayinkascott@gmail.com>
2023-02-08 01:40:07 +0000

    Task 2: changes made to textfile is null and file exists

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	o = open(filename, O_APPEND | O_WRONLY);
-	if (o == 1 && text_content == NULL)
+	if (o > 2 && text_content == NULL)
 		return (1);
 	if (o == -1 && text_content == NULL)
 		return (-1);
