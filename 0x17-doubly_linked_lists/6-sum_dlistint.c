49dc6c4c4c6d355609f93d09bc11842f4c098408
Andee <andeeolayinkascott@gmail.com>
2023-03-16 17:11:29 +0000

    Task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * sum_dlistint - this function sums up all the members in a doubly linked list
+  * @head: this is the first element in the doubly linked list
+  * Return: the sum of the data in the nodes or 0 if empty list
+  */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	int data, sum = 0;
+
+	temp = head;
+
+	/*loop through the entire doubly linked list*/
+	while (temp != NULL)
+	{
+		data = temp->n;
+		sum = sum + data;
+		temp = temp->next;
+	}
+	return(sum);
+}
