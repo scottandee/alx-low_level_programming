c657897797da6ea9206cbec4efe75a976703fc7b
Andee <andeeolayinkascott@gmail.com>
2023-03-16 16:51:12 +0000

    Task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
-	while (temp->next != NULL)
+	while (temp != NULL)
 	{
 		if (count == index)
 		{
