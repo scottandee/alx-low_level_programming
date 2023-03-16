3c295af989cadb4a63263f6b5c0a3fcab2212ca5
Andee <andeeolayinkascott@gmail.com>
2023-03-16 09:07:13 +0000

    Task 0

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _LISTS_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -18,5 +19,7 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
 
 #endif /* _LISTS_H */
