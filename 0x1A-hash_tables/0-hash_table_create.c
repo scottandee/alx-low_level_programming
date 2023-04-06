833b214b13b415fd3ae61d4e83cf28cd38767b75
Andee <andeeolayinkascott@gmail.com>
2023-04-06 18:13:46 +0000

    Edited changes to memory allocation for table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	table->size = size;
-	table->array = (hash_node_t**) calloc(table->size, sizeof(hash_node_t*));
+	table->array = calloc(table->size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		return (NULL);
