94d5a0f270a6abdaafd2e6a56ad833f2d84e8178
Andee <andeeolayinkascott@gmail.com>
2023-04-06 10:06:41 +0000

    Task 0: function that creates a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_create - this creates and allocates memory for a hash table
+  * @size: this is the size of the table to be created
+  * Return: It returns the table if successful and NULL if not
+  */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
