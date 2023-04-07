34b3f0af10f1087f76788cb90d393707730c5a3c
Andee <andeeolayinkascott@gmail.com>
2023-04-07 06:46:52 +0000

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(new_node->key, key) == 0)
 		{
-			strcpy(ht->array[index]->value, value);
+			ht->array[index]->value= strdup(value);
 		}
 		else
 		{
