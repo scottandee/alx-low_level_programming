f1119e780e11a97b3b096f5c196e54f285b27d54
Andee <andeeolayinkascott@gmail.com>
2023-04-07 14:42:39 +0000

    Task 3: fixing errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
-	ht->array[index] = new_node;
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 
