1d6803355959c53ff42c27581bebc398913ddb71
Andee <andeeolayinkascott@gmail.com>
2023-04-07 14:36:23 +0000

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,19 +11,23 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-	hash_node_t *temp;
+	hash_node_t *temp, *new_node;
 
 	if (strcmp(key, "") == 0) /*check if key is empty*/
 	{
 		return (0);
 	}
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
 	index = hash_djb2((unsigned char *)key);
 	index = index % ht->size;
 	temp = ht->array[index];
-
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	ht->array[index] = new_node;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 
 	if (ht->array[index] == NULL)/*If the index spot is empty*/
 	{
@@ -34,12 +38,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		while (temp != NULL)
 		{
-			if (strcmp(new_node->key, key) == 0)
-			/*If only an update to key value is needed*/
+			if (strcmp(temp->key, key) == 0)
 			{
 				free(ht->array[index]->value);
 				ht->array[index]->value = strdup(value);
-				free(new_node);
 				return (1);
 			}
 			temp = temp->next;
