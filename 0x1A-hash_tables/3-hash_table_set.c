ed57fda8a5454c7334ccc60c7df6349ef5e07827
Andee <andeeolayinkascott@gmail.com>
2023-04-07 06:41:03 +0000

    Task 3: fixed some errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 	hash_node_t *current;
 
-	if (key == "")
+	if (strcmp(key,"") == 0)
 	{
 		return (0);
 	}
