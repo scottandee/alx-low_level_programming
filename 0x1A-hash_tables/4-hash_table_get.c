7dac7a5232e4c247711b943d327df39a0d370f50
Andee <andeeolayinkascott@gmail.com>
2023-04-07 20:03:07 +0000

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *temp;
 	unsigned long int index;
 
-	if (!ht || !key || !*key)
+	if (!ht || !key)
 	{
 		return (NULL);
 	}
@@ -26,10 +26,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	}
 	while (temp != NULL)
 	{
-		if (strcmp(ht->array[index]->key, key) == 0)
+		if (strcmp(temp->key, key) == 0)
 			return (temp->value);
 		temp = temp->next;
 	}
 	return (NULL);
-
 }
