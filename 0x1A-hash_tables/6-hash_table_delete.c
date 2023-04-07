3a1865ff190ef373a02c3e09ef42274cdbe73d90
Andee <andeeolayinkascott@gmail.com>
2023-04-07 19:14:50 +0000

    Fixed error

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,7 +10,7 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *temp, *buffer;
 
-	for (i = 0; i <= table->size; i++)
+	for (i = 0; i <= ht->size; i++)
 	{
 		temp = ht->array[i];
 		while (temp != NULL)
