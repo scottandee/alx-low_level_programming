6f1253d0871ef410d2f3a26f78067d2af63a2fea
Andee <andeeolayinkascott@gmail.com>
2023-04-07 15:27:29 +0000

    Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_get - this function gets the value of the key given
+  * @ht: this is the hash table
+  * @key: this is the key that we're to retrieve it's value
+  * Return: Value if found, NULL if NOT
+  */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int index;
+
+	if (!ht || !key || !*key)
+	{
+		return (NULL);
+	}
+	index = hash_djb2((unsigned char *) key);
+	index = index % ht->size;
+	temp = ht->array[index];
+
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	while (temp != NULL)
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+
+}
