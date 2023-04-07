193a3719e96775d116ddec8596609ecce4364769
Andee <andeeolayinkascott@gmail.com>
2023-04-07 03:39:33 +0000

    Task 3: creating an insert function for hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_set - this adds an element to the hash table
+  * @ht: the hash table you want to add/update key/value
+  * @key: this is the key that will be used to search for a value
+  * @value: this is the value that is to be added at the index
+  * Return: 1 if sucess, 0 if failure
+  */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *current;
+
+	if (!key)
+	{
+		return (0);
+	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	index = hash_djb2((unsigned char *)key);
+	index = index % ht->size;
+	current = ht->array[index];
+
+
+	if (current == NULL)
+	{
+		current = new_node;
+		return (1);
+	}
+	else
+	{
+		if (strcmp(new_node->key, key) == 0)
+		{
+			strcpy(current->value, value);
+		}
+		else
+		{
+			new_node->next = current;
+			ht->array[index] = new_node;
+		}
+	}
+	return (1);
+}
