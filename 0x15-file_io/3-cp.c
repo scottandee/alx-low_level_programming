1d145f11da830c615947993833ad7551635a56a6
Andee <andeeolayinkascott@gmail.com>
2023-06-03 15:03:56 +0000

    Made cp function recursive

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,8 +4,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-void read_from(const char *file_from, char *buffer);
-void write_to(const char *file_to, char *buffer);
+int read_from(const char *file_from, char *buffer, int count);
+int write_to(const char *file_to, char *buffer, int count);
 void cp(const char *file_from, const char *file_to);
 
 /**
@@ -33,11 +33,21 @@ int main(int argc, char **argv)
 void cp(const char *file_from, const char *file_to)
 {
 	char *buffer;
+	int read = 1, count = 0, written = 0;
 
 	buffer = malloc(1024);
 
-	read_from(file_from, buffer);
-	write_to(file_to, buffer);
+	while (read != 0)
+	{
+		count += 1;
+		read = read_from(file_from, buffer, count);
+		printf("read: %d\n", read);
+		if (read != 0)
+		{
+			written = write_to(file_to, buffer, count);
+			printf("written: %d\n", written);
+		}
+	}
 	free(buffer);
 }
 
@@ -45,12 +55,18 @@ void cp(const char *file_from, const char *file_to)
   * read_from - this reads from file from and strores in the buffer
   * @file_from: this is the file it will read from
   * @buffer: this is the temporary storage
+  * @count: this is the number of times the file has beeen read
+  * Return: this function returns the number of bytes read
   */
-void read_from(const char *file_from, char *buffer)
+int read_from(const char *file_from, char *buffer, int count)
 {
-	int o, r, c;
+	int o, r, c, seek = 1024 * (count - 1);
 
 	o = open(file_from, O_RDONLY);
+	if (count != 1)
+	{
+		lseek(o, seek, SEEK_SET);
+	}
 	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
@@ -70,18 +86,28 @@ void read_from(const char *file_from, char *buffer)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o);
 		exit(100);
 	}
+	return (r);
 }
 
 /**
   * write_to - this writes from the temporary storage to the file_to
   * @file_to: this is the file that buffer will be written to
-  * @buffer: this is the temporary storage
+  * @buffer: this is the temporary storagei
+  * @count: this is the number of times the file has beeen read
+  * Return: this function returns the number of bytes written
   */
-void write_to(const char *file_to, char *buffer)
+int write_to(const char *file_to, char *buffer, int count)
 {
 	int o, w, c, len = 0;
 
-	o = open(file_to,  O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (count == 1)
+	{
+		o = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	}
+	else
+	{
+		o = open(file_to, O_WRONLY | O_APPEND);
+	}
 	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
@@ -104,4 +130,6 @@ void write_to(const char *file_to, char *buffer)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o);
 		exit(100);
 	}
+
+	return (w);
 }
