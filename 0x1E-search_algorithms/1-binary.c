22af517ddd39afc842e78277dde80739360b1c9e
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-09-13 19:06:37 +0100

    Add fix for edge case

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,6 +14,11 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t l = 0, r = size - 1, m, i;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
 	while (l <= r)
 	{
 		printf("Searching in array: ");
