626e7f4b3aef2c4fc30663c3b938642951b5ed9f
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-09-14 16:36:25 +0100

    Header file added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,11 @@
 #ifndef SEARCH_ALGOS_H_
 #define SEARCH_ALGOS_H_
 
+#include <math.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H_ */
